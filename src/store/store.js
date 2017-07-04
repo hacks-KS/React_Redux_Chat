2f9b7fbd4eb20d7fe1e8457251d869589b2f7c39
hacks-KS <b1013027@gamil.com>
2017-07-04 16:47:48 +0100

    Add: local program

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import {createLogger} from 'redux-logger';
 
 //reduxのlogを残してくれるようなstoreの作成
 
-export default function configureStore() {
+export default function configureStore() {
   const logger = createLogger({logger:console});
   const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
   const store = createStoreWithMiddleware(commentReducer);
